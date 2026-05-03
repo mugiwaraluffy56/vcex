64be08013d0f937fb59f6fb98af64d8d3bd81df5
puneeth_aditya_5656 <myakampuneeth@gmail.com>
2026-05-04 02:25:48 +0530

    Show server startup details

--- lib/lan_call/application.ex
+++ lib/lan_call/application.ex
@@ -3,13 +3,45 @@ defmodule LanCall.Application do
 
   @impl true
   def start(_type, _args) do
+    start_server? = Application.get_env(:lan_call, :start_server, true)
+
     children =
-      if Application.get_env(:lan_call, :start_server, true) do
+      if start_server? do
         [LanCall.Room, LanCall.Server]
       else
         []
       end
 
-    Supervisor.start_link(children, strategy: :one_for_one, name: LanCall.Supervisor)
+    case Supervisor.start_link(children, strategy: :one_for_one, name: LanCall.Supervisor) do
+      {:ok, _pid} = result ->
+        if start_server? do
+          LanCall.Banner.print(configured_port())
+        end
+
+        result
+
+      {:error, {:shutdown, {:failed_to_start_child, LanCall.Server, {:port_in_use, port}}}} ->
+        IO.puts("""
+        Port #{port} already in use.
+
+        Stop old server:
+        lsof -tiTCP:#{port} -sTCP:LISTEN | xargs kill
+
+        Or use another port:
+        PORT=4050 mix run --no-halt
+        """)
+
+        {:error, {:port_in_use, port}}
+
+      other ->
+        other
+    end
+  end
+
+  defp configured_port do
+    case System.get_env("PORT") do
+      nil -> Application.get_env(:lan_call, :port, 4000)
+      value -> String.to_integer(value)
+    end
   end
 end
